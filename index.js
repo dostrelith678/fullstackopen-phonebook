238b8b3c5fc2d401dfa8b3591497be64b7e77860
Dostrelith <12885604+dostrelith678@users.noreply.github.com>
2024-11-28 00:43:11 +0000

    ex 3.15

--- index.js
+++ index.js
@@ -33,13 +33,18 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(500).end()
+    })
 })
 
 app.post('/api/persons', (request, response) => {
@@ -64,9 +69,13 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  persons = persons.filter(person => person.id !== request.params.id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(500).end()
+    })
 })
 
 const PORT = process.env.PORT || 3001
